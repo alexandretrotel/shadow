9f463370f1b9bd774a8eb44e15d0f1c7af8522d2
Alexandre Trotel <trotelalexandre@proton.me>
2025-04-01 13:25:21 +0200

    feat: integrate VitePWA for enhanced PWA support and add robots.txt for SEO

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,42 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Shadow Chat",
+        short_name: "Shadow",
+        description: "Whisper in the Dark – Secure, Ephemeral, Unseen.",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
