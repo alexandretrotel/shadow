9bcb9114d43f66dfea4e777bdae22918a77a85dc
Alexandre Trotel <trotelalexandre@proton.me>
2025-04-01 21:59:04 +0200

    feat: set up routing and socket handling in the server index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import { setupRoutes } from "./routes";
+import { setupSockets } from "./sockets";
 
 const app = express();
 app.use(
