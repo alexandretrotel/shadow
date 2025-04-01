e37602fa22bd7508323f1947bfc2e673788f19c6
Alexandre Trotel <trotelalexandre@proton.me>
2025-04-01 19:15:11 +0200

    feat: remove old database configuration files and add new drizzle configuration

--- shared/db/schema.ts
+++ shared/db/schema.ts
@@ -13,8 +13,19 @@ export const users = pgTable(
     publicKey: text("public_key").notNull().unique(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
   },
-  (users) => [
-    primaryKey({ columns: [users.username, users.publicKey] }),
-    uniqueIndex("username_idx").on(users.username),
-  ]
+  (users) => [primaryKey({ columns: [users.username, users.publicKey] })]
+);
+
+export const contacts = pgTable(
+  "contacts",
+  {
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.username),
+    contactId: text("contact_id")
+      .notNull()
+      .references(() => users.username),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (contacts) => [primaryKey({ columns: [contacts.userId, contacts.contactId] })]
 );
